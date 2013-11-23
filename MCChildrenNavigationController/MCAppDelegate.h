aec7602764c6695d2030a90a3a07528349c6533f
Miguel Cabeça <cabeca@ist.utl.pt>
2013-11-23 15:59:13 +0000

    Initial Commit

---
+++ MCChildrenNavigationController/MCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCAppDelegate.h
+//  MCChildrenNavigationController
+//
+//  Created by Miguel Cabeça on 23/11/13.
+//  Copyright (c) 2013 Miguel Cabeça. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MCChildrenNavigationController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MCChildrenNavigationController
+//
+//  Created by Miguel Cabeça on 23/11/13.
+//  Copyright (c) 2013 Miguel Cabeça. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCAppDelegate class]));
+    }
+}
